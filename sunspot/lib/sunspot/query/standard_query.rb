6063b05a30385055eea76da5656779cd13d3b0b4
Rob Kaufman <rob@notch8.com>
2011-01-13 19:35:08 -0600

    Revert "Remove solr-spatial-light support, configuration, libraries"
    This reverts commit 372e55d89f5d3d4dd785405ddd1db8e3ddf1a936.
    
    Conflicts:
    
    	sunspot/lib/sunspot/dsl/standard_query.rb

--- sunspot/lib/sunspot/query/standard_query.rb
+++ sunspot/lib/sunspot/query/standard_query.rb
@@ -11,6 +11,10 @@ module Sunspot
       def add_fulltext(keywords)
         @fulltext.add(keywords)
       end
+
+      def add_location_restriction(coordinates, radius)
+        @components << @local = Local.new(coordinates, radius)
+      end
     end
   end
 end
